Day 6 Part 2 : correct normalization 5588 too low

Day 6/Part two/TestDay5part2.py
@@ -144,14 +144,14 @@ def day_6_part_2(lines):
     # factor for the margin if our grid is 3*3 and
     # the factor is 2 we obtains 9*9 with the areas
     # on the middle of the grid
-    factor_to_finite = 2
+    factor_to_finite = 1
     # get_areas_and_dimension
     maxx, maxy, areas = init_areas_with_bounds(lines)
     # add area for a special grid using a factor for the margin
     grid = np.zeros((maxy*factor_to_finite, maxx*factor_to_finite), dtype=int)
-    """
     # set areas coordonate before add them
     areas = normalize_areas(maxx, maxy, factor_to_finite, areas)
+    """
     # transform the grid with nearest point
     grid = set_nearest_point(grid, areas)
     # get all probably infinite areas on the grid borders