9393cd0838ca2d6654f3ba51f46d4d0b6e49e3e9
jnguyen <johdu13@hotmail.fr>
2018-12-06 23:28:00 +0100

    Day 6 Part 1 : Refactoring

--- Day 6/Day  6 - Chronal Coordinates/TestDay6part1.py
+++ Day 6/Day  6 - Chronal Coordinates/TestDay6part1.py
@@ -1,8 +1,6 @@
 import unittest
 import numpy as np
 
-factor_to_finite = 2
-
 
 def input_file():
     # return the input file in a text
@@ -26,15 +24,27 @@ class Area:
         self.x = x
         self.y = y
 
-    def get_size_area_from_other_points(self, areas):
-        # calcul the size area from other points
-        return 0
-
     def get_manhattan_distance(self, x, y):
+        # return the manhattan distance between two points
         return abs(self.x - x) + abs(self.y - y)
 
 
+def init_areas_with_bounds(lines):
+    # create the areas with the dimension of grid
+    areas = []
+    maxx, maxy = (0, 0)
+    for line in lines:
+        x, y = ([int(l) for l in line.split(',')])
+        areas.append(Area(x, y))
+        if maxx < x:
+            maxx = x
+        if maxy < y:
+            maxy = y
+    return maxx, maxy, areas
+
+
 def get_nearest_point(x, y, areas):
+    # return the nearest point from a position if there are equals we put a 0 as a dot
     d_min = np.inf
     # each distances to case dot
     distances = []
@@ -74,35 +84,17 @@ def pretty_print(areas, grid):
     for index, area in enumerate(areas):
         grid_char[area.y][area.x] = alpha[index].upper()
     print(grid_char)
-    #np.savetxt("test.out", grid.astype(int), fmt='%i', delimiter=' ')
 
 
-def day_6_part_1(lines):
-    # get_areas_and_dimension
-    areas = []
-    maxx, maxy = (0, 0)
-    for line in lines:
-        x, y = ([int(l) for l in line.split(',')])
-        areas.append(Area(x, y))
-        if maxx < x:
-            maxx = x
-        if maxy < y:
-            maxy = y
-    # add area for a special grid
-    grid = np.zeros((maxy*factor_to_finite, maxx*factor_to_finite), dtype=int)
-    # set areas coordonate before add them
+def normalize_areas(maxx, maxy, factor_to_finite, areas):
     for area in areas:
         area.x = area.x + int((maxx*factor_to_finite)/2)
         area.y = area.y + int((maxy*factor_to_finite)/2)
+    return areas
 
-    # pour chaque cellule qui valent 0 faire un calcul pour definir quel chiffre est le plus proche
-    # s il y a egalite mettre un point soit 0
-    for y in range(grid.shape[0]):
-        for x in range(grid.shape[1]):
-            # get nearest point and put the good point or dot
-            grid[y][x] = get_nearest_point(x, y, areas)
 
-    # all_bound_areas = get_all_bound_areas()
+def get_all_borders_areas(grid):
+    # return all the areas on the borders
     left_bound_areas = list(set(grid[:, 1]))
     left_bound_areas.remove(52)
     top_bound_areas = list(set(grid[:, 1]))
@@ -111,22 +103,59 @@ def day_6_part_1(lines):
     bot_bound_areas.remove(52)
     right_bound_areas = list(set(grid[:, grid.shape[1]-1]))  # shape[1] line
     right_bound_areas.remove(52)
-    all_bound_areas = list(set(left_bound_areas+top_bound_areas+bot_bound_areas+right_bound_areas))
-    #print("all_bound_areas ", all_bound_areas)
-    # finite_areas = get_finite_areas()
-    num_finite_areas = list(set(list(range(len(areas)))) - set(all_bound_areas))
-    #print("finite_areas ", num_finite_areas)
-    # count in matrice
-    #count = len(np.where(grid == 2)[0])
-    # best_area_size = get_best_area_size(finite_areas)
+    return list(set(left_bound_areas+top_bound_areas+bot_bound_areas+right_bound_areas))
+
+
+def set_nearest_point(grid, areas):
+    # pour chaque cellule qui valent 0 faire un calcul pour definir quel chiffre est le plus proche
+    # s il y a egalite mettre un point soit 0
+    for y in range(grid.shape[0]):
+        for x in range(grid.shape[1]):
+            # get nearest point and put the good point or dot
+            grid[y][x] = get_nearest_point(x, y, areas)
+    return grid
+
+
+def get_num_probably_finite_areas(all_bound_areas, areas):
+    # return the number of the probably finite areas because there was some exceptions
+    return list(set(list(range(len(areas)))) - set(all_bound_areas))
+
+
+def count_number_of_n_in_grid(n, grid):
+    # return the size of area
+    return len(np.where(grid == n)[0])
+
+
+def get_best_area_size(num_probably_finite_areas, grid, maxx, maxy):
     best_area_size = 0
-    for num_finite_area in num_finite_areas:
-        finite_area_size = len(np.where(grid == num_finite_area)[0])
-        if finite_area_size > best_area_size and finite_area_size < 9500:
+    for num_finite_area in num_probably_finite_areas:
+        finite_area_size = count_number_of_n_in_grid(num_finite_area, grid)
+        # the area is fixed on the previous try which was 9674
+        if best_area_size < finite_area_size < 9674:
             best_area_size = finite_area_size
-    print("best_area_size ", best_area_size)
-    #pretty_print(areas, grid)
-    return ""
+    return best_area_size
+
+
+def day_6_part_1(lines):
+    # factor for the margin if our grid is 3*3 and
+    # the factor is 2 we obtains 9*9 with the areas
+    # on the middle of the grid
+    factor_to_finite = 2
+    # get_areas_and_dimension
+    maxx, maxy, areas = init_areas_with_bounds(lines)
+    print(maxx)
+    print(maxy)
+    # add area for a special grid using a factor for the margin
+    grid = np.zeros((maxy*factor_to_finite, maxx*factor_to_finite), dtype=int)
+    # set areas coordonate before add them
+    areas = normalize_areas(maxx, maxy, factor_to_finite, areas)
+    # transform the grid with nearest point
+    grid = set_nearest_point(grid, areas)
+    # get all probably infinite areas on the grid borders
+    all_probably_finite_areas_on_borders = get_all_borders_areas(grid)
+    num_probably_finite_areas = get_num_probably_finite_areas(all_probably_finite_areas_on_borders, areas)
+    best_area_size = get_best_area_size(num_probably_finite_areas, grid, maxx, maxy)
+    return str(best_area_size)
 
 
 class TestDay6part1(unittest.TestCase):
@@ -134,10 +163,10 @@ class TestDay6part1(unittest.TestCase):
     def test_day_6_part_1(self):
         lines = input_file()
         lines.sort()
-        #res = output_file()
+        res = output_file()
         pred = day_6_part_1(lines)
         print(pred)
-        #assert(pred == res[0])
+        assert(pred == res[0])
 
 
 if __name__ == '__main__':
