786687b7dc4c69c122c8941e3b570b7ff83b1b27
jnguyen <johdu13@hotmail.fr>
2018-12-08 00:53:49 +0100

    Day 7 part 1: Good answer BITRAQVSGUWKXYHMZPOCDLJNFE

--- Day 7/Day  7 - The Sum of Its Parts/TestDay7part1.py
+++ Day 7/Day  7 - The Sum of Its Parts/TestDay7part1.py
@@ -1,7 +1,6 @@
 import unittest
-import numpy as np
 from parse import parse
-from pprint import pprint
+
 
 def input_file():
     # return the input file in a text
@@ -19,157 +18,115 @@ def output_file():
     return res
 
 
-class Node_before:
-    def __init__(self, before, current):
-        self.before = before
-        self.current = current
+class Correct_order:
+    # class to execute the correct order list
+    def __init__(self, raw_nodes, first_letters):
+        self.raw_nodes = raw_nodes
+        self.available_letters = sorted(first_letters)
+        self.correct_order_string = ""
 
-    def get_current_value(self):
-        return self.current - 1
+    def get_available_letters(self):
+        # return available letters
+        return self.available_letters
 
-    def get_before(self):
-        return self.before
+    def add_available_letter(self, available_letter):
+        # add an available letter in our list of letter sorted
+        self.available_letters = list(sorted(set(self.available_letters + [available_letter])))
 
-    def get_current(self):
-        return self.current
+    def add_correct_order(self, letter):
+        # add an available letter in our list of letter sorted
+        self.correct_order_string = self.correct_order_string + str(letter)
 
+    def get_prerequite_letters(self, letter):
+        # return all prerequite letters
+        prerequite_letters = []
+        for raw in self.raw_nodes:
+            if raw[1] == letter:
+                prerequite_letters.append(raw[0])
+        return prerequite_letters
 
-class Ordered_nodes:
-
-    def __init__(self, first_letter, nodes):
-        self.letters = [first_letter]
-        self.values = [0]
-        self.nodes = nodes
-
-    def add_node_before(self, node):
-        letter = node.get_before()
-
-        # case letter already exist
-        if node.get_before() in self.letters:
-            # decrement its value
-            self.decrement_value_using_letter(letter)
-        # case current letter doesn't exist
-        if node.get_current() not in self.letters:
-            # nothing to do
-            return False
-        # case before letter doesn't exist
-        else:
-            # TODO order by [1] or [0] the raw_nodes
-            # add on letters with value of current letter - 1
-            self.letters.append(node.get_before())
-            current_value_less_one = self.get_value_using_letter(node.get_current()) - 1
-            self.values.append(current_value_less_one)
+    def is_all_prerequite(self, prerequite_letters):
+        # constraint with all prerequite
+        available_letters = self.get_available_letters()
+        for letter in prerequite_letters:
+            if letter in available_letters:
+                return False
+            elif letter not in self.get_correct_order_string():
+                return False
         return True
 
-    def get_value_using_letter(self, letter):
-        # get letter num
-        letter_num = 0
-        for index, value in enumerate(self.letters):
-            if letter == value:
-                letter_num = index
-        return self.values[letter_num]
+    def get_available_letter_without_preprequite_letters(self):
+        # return avaible letter respecting the constraint of prerequite letters
+        available_letters = self.get_available_letters()
+        for available_letter in available_letters:
+            prerequite_letters = self.get_prerequite_letters(available_letter)
+            if self.is_all_prerequite(prerequite_letters):
+                return available_letter
+        return available_letters[0]
 
-    def decrement_value_using_letter(self, letter):
-        # get letter num
-        letter_num = 0
-        for index, value in enumerate(self.letters):
-            if letter == value:
-                letter_num = index
-        self.values[letter_num] = self.values[letter_num] - 1
+    def get_available_letter(self):
+        # return the available letter order by alpha
+        available_letter = self.get_available_letter_without_preprequite_letters()
+        # remove the letter
+        self.available_letters.remove(available_letter)
+        # add this letter to the correct order list
+        self.add_correct_order(available_letter)
+        return available_letter
 
-    def execute_logic(self):
-        # stop when there wasn't nodes to analyse
-        while len(self.nodes) != 0:
-            for index, node in enumerate(self.nodes):
-                is_proceed = self.add_node_before(node)
-                if is_proceed:
-                    del self.nodes[index]
+    def next_step(self):
+        # get_child of next available
+        available_letter = self.get_available_letter()
+        # add all child on available list
+        for raw in self.raw_nodes:
+            if raw[0] == available_letter:
+                self.add_available_letter(raw[1])
 
-    def get_ordered_instructions(self):
-        # init instructions
-        instructions = ""
-        # init dictionnary
-        dictionary = {}
-        for index, letter in enumerate(self.letters):
-            dictionary[letter] = self.values[index]
-        pprint(dictionary)
-        # sort dictionnary by value https://stackoverflow.com/questions/20944483/python-3-sort-a-dict-by-its-values
-        s = [(k, dictionary[k]) for k in sorted(dictionary, key=dictionary.get, reverse=True)]
-        for k, v in s:
-            # add into instruction
-            instructions += k
-            # TODO ordered by letter order
-        return instructions[::-1]
-        # KS
+    def exec(self):
+        # execute the brute force
+        while len(self.available_letters) != 0:
+            self.next_step()
+
+    def get_correct_order_string(self):
+        # return the correct order string
+        return self.correct_order_string
+
+
+def get_first_letters(letters, raw_nodes):
+    # return the first letters of the workflow
+    first_letters = []
+    # find all first value of the worflow
+    for letter in letters:
+        find_value = False
+        for raw in raw_nodes:
+            if letter == raw[1]:
+                find_value = True
+                break
+        if not find_value:
+            first_letters.append(letter)
+    return first_letters
+
 
 def day_7_part_1(text):
-    # TODO
     # data prepare
     raw_nodes = [tuple(parse("Step {} must be finished before step {} can begin.", l)) for l in text.split('\n')] # @source https://github.com/ngilles/adventofcode-2018/blob/master/day-04/day-04.py
-    pprint(raw_nodes)
-    nodes = []
-    # create nodes
-    for raw_node in raw_nodes:
-        nodes.append(Node_before(raw_node[0], raw_node[1]))
-    #letters_to_test
-    letters_to_test = set()
-    # get letter to test
-    for raw_node in raw_nodes:
-        letters_to_test.add(raw_node[0])
-        letters_to_test.add(raw_node[1])
-    # get letter at the end
-    for last_probably_letter in letters_to_test:
-        find_last_letter = True
-        for couple in raw_nodes:
-            if couple[0] == last_probably_letter:
-                find_last_letter = False
-                break
-        if find_last_letter:
-            print("last_probably_letter ", last_probably_letter)
-    # search model
-    # affect value on E equals 0
-    # search before letter of E
-    # affect value on before letter of E
-    ordered_nodes = Ordered_nodes("E", nodes)
-    #
-    # couple (C, A)
-    """
-    E = 0
-    B = E - 1
-    D = E - 1
-    F = E - 1
-    A = B - 1
-    A = D - 1
-    C = A - 1
-    C = F - 1
-    """
-    # affect each variable a value
-    # TODO swap variable
-    # execute logic
-    ordered_nodes.execute_logic()
-    # print ordered_instruction
-    """print(C)
-    print(A)
-    print(B)
-    print(D)
-    print(F)
-    print(E)
-    """
-    return ordered_nodes.get_ordered_instructions()
+    # data transform
+    letters = set([raw[0] for raw in raw_nodes] + [raw[1] for raw in raw_nodes])
+    first_letters = get_first_letters(letters, raw_nodes)
+    # data model
+    correct_order = Correct_order(raw_nodes, first_letters)
+    # data analysis
+    correct_order.exec()
+    # data visualize
+    return correct_order.get_correct_order_string()
 
 
 class TestDay7part1(unittest.TestCase):
 
     def test_day_7_part_1(self):
         text = input_file()
-        #lines.sort()
-        #res = output_file()
+        res = output_file()
         pred = day_7_part_1(text)
-        print("less to zero")
-        for i in range(-4, 0):
-            print("i ", i)
-        print(pred)
-        #assert(pred == res[0])
+        assert(pred == res[0])
 
 
 if __name__ == '__main__':
