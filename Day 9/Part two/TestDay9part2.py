fdeec0cf794d51446c2b4b8d4bf4cdeae04cea65
jnguyen <johdu13@hotmail.fr>
2018-12-09 17:51:17 +0100

    Day 9 part 1: building

--- Day 9/Part two/TestDay9part2.py
+++ Day 9/Part two/TestDay9part2.py
@@ -19,6 +19,51 @@ def output_file():
     return res
 
 
+class Player:
+    # class that representing each player with his numero and his score
+    def __init__(self, numero, score=0):
+        self.num = numero
+        self.score = score
+
+
+class MarbleGame:
+    # class that representing the game with the list of marble, the current position, the players, the limit of last marble points and the associatives rules
+    def __init__(self, players, limit_marble_points=5, marbles=[0], current_marble=0, num_turn_player=0):
+        self.players = players
+        self.limit_marble_points = limit_marble_points
+        self.marbles = marbles
+        self.current_marble = current_marble # from 0 to N
+        self.num_turn_player = num_turn_player
+
+    def add_point(self):
+        # TODO add points to player
+        pass
+
+    def remove_marble(self):
+        # TODO remove the marble from the list
+        pass
+
+    def insert_marble(self, player):
+        # rule : a player insert a marble with the next marble in clockwise
+        self.next_marble()
+
+        self.next_marble()
+        # rule : something differently happens for multiple of 23
+        # keep marble that would be placed to points
+        # marble 7 in counter-clockwise from the current marble is removed and add to points
+        # immediatly located clockwise after the marble removed and become the new current marble plus one
+
+    def next_marble(self):
+        # rule : circle, increment by one until every marble has a number
+        if self.current_marble % len(self.marbles) - 1 == 0:
+            self.current_marble = 0
+        else:
+            # move to one on clockwise
+            self.current_marble += 1
+
+
+
+
 def day_9_part_2(text):
     # data retrieve
     raw_nodes = [tuple(parse("Step {} must be finished before step {} can begin.", l)) for l in
