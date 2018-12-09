fb92e12919e50be24a85315c88e640bd12cd459f
jnguyen <johdu13@hotmail.fr>
2018-12-09 14:07:40 +0100

    Day 9 : preparation

---
+++ Day 9/Part two/TestDay9part2.py
@@ -0,0 +1,40 @@
+import unittest
+import string
+from parse import parse
+
+
+def input_file():
+    # return the input file in a text
+    file = open('input', 'r')
+    text = file.read()
+    file.close()
+    return text
+
+
+def output_file():
+    # read line of output file
+    file = open('output', 'r')
+    res = [line.rstrip('\n') for line in file]
+    file.close()
+    return res
+
+
+def day_9_part_2(text):
+    # data retrieve
+    raw_nodes = [tuple(parse("Step {} must be finished before step {} can begin.", l)) for l in
+                 text.split('\n')]  # @source https://github.com/ngilles/adventofcode-2018/blob/master/day-04/day-04.py
+
+    return str(0)
+
+
+class TestDay9part2(unittest.TestCase):
+
+    def test_day_9_part_2(self):
+        text = input_file()
+        res = output_file()
+        pred = day_9_part_2(text)
+        assert(pred == res[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
