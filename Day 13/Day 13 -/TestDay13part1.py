9f899d6d2d1a60d8fa250421d1e2fec7790063a8
jnguyen <johdu13@hotmail.fr>
2018-12-15 11:07:52 +0100

    Day 13 part 1: Good answer 79,128 because we need to update the order of carts'move each step thx to purple_gauss from https://www.reddit.com/r/adventofcode/comments/a5yowv/day_13_number_of_ticks_part_1/

--- Day 13/Day 13 -/TestDay13part1.py
+++ Day 13/Day 13 -/TestDay13part1.py
@@ -22,7 +22,7 @@ class Cart:
     A cart is represented by his coordonate and his direction,
     its moves are specific during an intersection.
     """
-    def __init__(self, y, x, direction, map):
+    def __init__(self, y, x, direction, map, per_intersection_memory=0):
         """
 
         :param y: the coordonate y from 0 to n to the down
@@ -33,7 +33,7 @@ class Cart:
         self.x = x
         self.direction = direction
         self.map = map
-        self.per_intersection_memory = 0
+        self.per_intersection_memory = per_intersection_memory
 
     def get_direction(self):
         """
@@ -41,6 +41,10 @@ class Cart:
         :return: direction of the cart to know where it will go
         """
         return self.direction
+
+    def get_per_intersection_memory(self):
+        # get the current intersection memory
+        return self.per_intersection_memory
 
     def get_position_yx(self):
         """
@@ -88,46 +92,27 @@ class Cart:
     def go_right(self):
         current_direction = self.get_direction()
         if current_direction == ">":
-            #self.go_south()
             self.direction_south()
         elif current_direction == "^":
-            #self.go_east()
             self.direction_east()
         elif current_direction == "<":
-            #self.go_north()
             self.direction_north()
         else:
-            #self.go_west()
             self.direction_west()
 
     def go_straight(self):
-        current_direction = self.get_direction()
-        if current_direction == ">":
-            #self.go_east()
-            self.direction_east()
-        elif current_direction == "^":
-            #self.go_north()
-            self.direction_north()
-        elif current_direction == "<":
-            #self.go_west()
-            self.direction_west()
-        else:
-            #self.go_south()
-            self.direction_south()
+        # nothing to change
+        pass
 
     def go_left(self):
         current_direction = self.get_direction()
         if current_direction == ">":
-            #self.go_north()
             self.direction_north()
         elif current_direction == "^":
-            #self.go_west()
             self.direction_west()
         elif current_direction == "<":
-            #self.go_south()
             self.direction_south()
         else:
-            #self.go_east()
             self.direction_east()
 
     def incr_per_intersection_memory(self):
@@ -166,7 +151,6 @@ class Cart:
                 self.direction_north()
             else:
                 self.direction_east()
-        #print(self.get_direction())
 
     def move(self):
         """
@@ -239,7 +223,14 @@ class ObjectBuilder:
         for cart in carts:
             # create each cart object
             carts_object.append(Cart(cart[0], cart[1], cart[2], self.map))
+        # remove the carts from the map
+        self.remove_carts_from_map(carts_object)
         return carts_object
+
+    def remove_carts_from_map(self, carts_object):
+        for cart in carts_object:
+            print("cart removed")
+            self.map[cart.get_position_yx()[0]][cart.get_position_yx()[1]] = " "
 
     def build_map(self):
         # return the matrix of the map
@@ -310,59 +301,90 @@ class MineCartMadnessManager:
         elif curr_direction == "<":
             next_position = curr_position[0], curr_position[1] - 1
         elif curr_direction == "v":
-            next_position = curr_position[0] + 1, curr_position[1] - 1
+            next_position = curr_position[0] + 1, curr_position[1]
         return next_position
 
-    def will_collision(self, cart):
+    def other_cars(self, cart):
+        """
+        Get list the of other carts
+        :param cart:
+        :return: all carts except the current cart
+        """
+        from copy import deepcopy
+        other_carts = deepcopy(self.carts)
+        for c in other_carts:
+            if c.get_position_yx() == cart.get_position_yx():
+                other_carts.remove(c)
+        return other_carts
+
+    def are_they_on_collision(self, cart):
         """
         We know if it will be a collision in the next move
         :param cart: the current cart
-        :return: True if there was a cart in front of the current cart
+        :return: True if there was a cart with the current cart position
         """
 
         # get the next position of current cart
-        next_position = self.get_next_position_cart(cart)
+        #next_position = self.get_next_position_cart(cart)
         current_position = cart.get_position_yx()
+        two_cars = 0
         # browse in carts
         for cart in self.carts:
             # if there was a cart here return True
-            # First case in the right position they enter on collision
-            if next_position == cart.get_position_yx():
-                return True
-            # Second case at the same time they enter on collision
-            if next_position == self.get_next_position_cart(cart) and current_position != cart.get_position_yx():
-                print("boom")
-                return True
+            if current_position == cart.get_position_yx():
+                two_cars += 1
+        if two_cars == 2:
+            self.collision_position = current_position
+            return True
         return False
+
+    def sort_carts(self):
+        carts_to_sorted = []
+        for cart in self.carts:
+            carts_to_sorted.append(tuple((cart.get_position_yx()[0], cart.get_position_yx()[1], cart.get_direction(), cart.get_per_intersection_memory())))
+        carts_to_sorted = sorted(carts_to_sorted)
+        print(carts_to_sorted)
+        self.carts = []
+        # browse on list of tuple
+        for cart in carts_to_sorted:
+            # create each cart object
+            self.carts.append(Cart(cart[0], cart[1], cart[2], self.map, cart[3]))
+
 
     def run(self):
         # launch the execution of cart on the map only if there was no collision
-        turn = 0
+        turn = 2
+        pos_x = (-1, -1)
         while not self.is_collision():
             # browse in each carts
-            #self.print_map()
-            #print(turn)
-            turn += 1
+            # refresh order
+            print(turn)
+            # sort carts
+            self.sort_carts()
+            #print(self.carts)
             for cart in self.carts:
                 # determine if it will be a collision between the current cart and another
-                if self.will_collision(cart):
+                if self.are_they_on_collision(cart):
                     # set the final parameter and leave the loop
-                    self.collision_position = cart.get_position_yx()
                     self.there_was_a_collision()
-                    self.print_map_with_carts(cart.get_position_yx())
+                    pos_x = cart.get_position_yx()
                     break
+                #self.print_map_with_carts(cart.get_position_yx())
                 cart.move()
-                #self.print_map_with_carts()
+            #self.print_map_with_carts(cart.get_position_yx())
+            #self.print_map_with_carts(pos_x)
+            turn += 1
 
-    def print_map_with_carts(self, pos_X):
+    def print_map_with_carts(self, pos_x=(-1, -1)):
         from copy import deepcopy
         map_with_carts = deepcopy(self.map)
 
         for cart in self.carts:
             y, x = cart.get_position_yx()
             map_with_carts[y][x] = cart.get_direction()
-        y, x = pos_X
-        map_with_carts[y][x] = "X"
+        y, x = pos_x
+        if y != -1:
+            map_with_carts[y][x] = "X"
         string_map = ""
         for line in map_with_carts:
             string_map += "".join(line) + "\n"
