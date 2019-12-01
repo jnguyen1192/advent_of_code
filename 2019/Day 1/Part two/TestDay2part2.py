bfd1fedc133d7dee4b7b7d8e28ec60cf343c808d
jnguyen <johdu13@hotmail.fr>
2019-12-01 05:54:21 +0100

    #4 Refactored

---
+++ 2019/Day 1/Part two/TestDay2part2.py
@@ -0,0 +1,79 @@
+import unittest
+
+
+def input_file():
+    # read lines of input file
+    file = open('input', 'r')
+    lines = [line.rstrip('\n') for line in file]
+    file.close()
+    return lines
+
+
+def output_file():
+    # read line of output file
+    file = open('output', 'r')
+    res = [line.rstrip('\n') for line in file]
+    file.close()
+    return res
+
+
+def count_similar_char(str1, str2):
+    # return the number of same occurence between two strings
+    # init acc
+    acc = 0
+    # browse in string length
+    for i in range(len(str1)):
+        # compare each char
+        if str1[i] == str2[i]:
+            # increment acc
+            acc += 1
+    return acc
+
+
+def sub_str(str1, str2):
+    # substract two string and give the final string result
+    # init str result
+    str_res = ""
+    # browse in string length
+    for i in range(len(str1)):
+        # compare each char
+        if str1[i] == str2[i]:
+            # add char to the final string result
+            str_res += str1[i]
+    return str_res
+
+
+def best_match_str(lines):
+    # return the two strings that matches the best except the same strings
+    # init variables
+    line1_res = ""
+    line2_res = ""
+    count_similar = 0
+    # browse lines of file
+    for line1 in lines:
+        # browse lines of file for each line
+        for line2 in lines:
+            # process only if lines are different
+            if line1 != line2:
+                # count
+                count = count_similar_char(line1, line2)
+                # get the best different strings except the same
+                if count > count_similar and count != len(line1):
+                    count_similar = count
+                    # keep result lines
+                    line1_res = line1
+                    line2_res = line2
+    return line1_res, line2_res
+
+
+class TestDay1part2(unittest.TestCase):
+
+    def test_day_1_part_2(self):
+        lines = input_file()
+        res = output_file()
+        line1_res, line2_res = best_match_str(lines)
+        assert(sub_str(line1_res, line2_res) == res[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
