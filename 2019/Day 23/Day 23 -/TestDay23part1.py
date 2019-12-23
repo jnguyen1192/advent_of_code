a10f973848a37d213a947b6395e331332736eb11
jnguyen1192 <johdu13@hotmail.fr>
2019-12-23 07:38:48 +0100

    #5 Day 23 part 1 Intcode refactored and still not working

--- 2019/Day 23/Day 23 -/TestDay23part1.py
+++ 2019/Day 23/Day 23 -/TestDay23part1.py
@@ -25,144 +25,102 @@ class TestDay23part1(unittest.TestCase):
     def test_day_23_part_1(self):
         text = input_file()
 
-        def process(phase_setting, value, i, code, phase):
-            length = len(code)
-            rb = 0
-            end = False
-            debug = ""
-            try:
-                while i < length or code[i] != 99:
-                    print(i)
-                    # add mode on opcode
-                    #print("i", i, length, code[i:i+10])
-                    if code[i] == 99:
-                        print("exit")
-                        end = True
+        class Intcode:
+            def __init__(self, code, i=0, phase=False, rb=0):
+                self.code = code + [0] * 100000
+                self.i = i
+                self.phase = phase
+                self.rb = rb
+
+            def run(self, input, phase_setting=None, output=None):
+                if phase_setting is not None:
+                    self.phase = True
+                while True:
+                    if self.code[self.i] == 99:
                         break
-                    debug = "before cast " + str(i) + " " + str(code[i]) + " " + str(len(code))
-                    tmp_cur = code[i]
-                    str_opcode = str(tmp_cur)
+                    str_opcode = str(self.code[self.i])
                     while len(str_opcode) < 5:
                         str_opcode = "0" + str_opcode
                     opcode = int(str_opcode[3:])
-                    # add pos and imm
+                    # add pos, imm and rela
                     # case param 1
                     if int(str_opcode[2]) == 0:  # position
-                        first_param = code[i + 1]
+                        first_param = self.code[self.i + 1]
                     elif int(str_opcode[2]) == 2:  # relative
-                        #print("relative mode", str(i + 1 + rb))
-                        if i + 2 + rb > length and code[i] != 4:
-                            code += [0] * (code[i + 1] + rb - len(code))
-                        #debug = "here " + str(i + 1 + rb) + " " + str(len(code))
-                        first_param = code[i + 1] + rb
+                        first_param = self.code[self.i + 1] + self.rb
                     else:
-                        first_param = i + 1
-                    debug = "first param " + str(first_param)
+                        first_param = self.i + 1
                     # case param 2
                     if int(str_opcode[1]) == 0:  # position
-                        second_param = code[i + 2]
+                        second_param = self.code[self.i + 2]
                     elif int(str_opcode[1]) == 2:  # relative
-                        #print("relative mode")
-                        second_param = code[i + 2] + rb
+                        second_param = code[self.i + 2] + self.rb
                     else:
-                        second_param = i + 2
-
-                    debug = "second param " + str(second_param)
-                    # case param 2
+                        second_param = self.i + 2
+                    # case param 3
                     if int(str_opcode[0]) == 0:  # position
-                        try:
-                            third_param = code[i + 3]
-                        except:
-                            third_param = 0
-                        debug = " third param " + str(third_param)
-                        debug += "\ncode[i] " + str(code[i])
-                        # TODO dynamic list extension here
-                        if third_param > length and code[i] != 4:
-                            code += [0] * (third_param - len(code))
+                        third_param = self.code[self.i + 3]
                     elif int(str_opcode[0]) == 2:  # relative
-                        third_param = code[i + 3] + rb
+                        third_param = self.code[self.i + 3] + self.rb
                     else:
-                        third_param = i + 3
-
-                    debug = "after params " + str(third_param) + " " + str(first_param) + " + " + str(second_param)
+                        third_param = self.i + 3
 
                     if opcode == 1:  # addition
-                        # case param 3
-                        debug = "addition " + str(third_param) + " " + str(first_param) + " + " + str(second_param) + " "\
-                                + str(len(code)) + " " + str(third_param) + " " + str(len(code) - third_param)
-                        if third_param >= len(code):
-                            code += [0] * (third_param + 1 - len(code))
-                        tmp = code[first_param] + code[second_param]
-                        code[third_param] = tmp
-                        i += 4
+                        tmp = self.code[first_param] + self.code[second_param]
+                        self.code[third_param] = tmp
+                        self.i += 4
                     elif opcode == 2:  # multiplication
                         # case param 3
-                        debug = "multiplication"
-                        tmp = code[first_param] * code[second_param]
-                        if third_param >= len(code):
-                            code += [0] * (third_param + 1 - len(code))
-                        code[third_param] = tmp
-                        i += 4
-                    # TODO code 3 and code 4
+                        tmp = self.code[first_param] * self.code[second_param]
+                        self.code[third_param] = tmp
+                        self.i += 4
                     elif opcode == 3:  # as input 3, 50 => 3 take input and save to address 50
-                        debug = "input"
-                        if phase:
-                            code[first_param] = phase_setting
-                            phase = False
+                        if self.phase:
+                            self.code[first_param] = phase_setting
+                            self.phase = False
                         else:
-                            code[first_param] = value
-                        i += 2
+                            self.code[first_param] = input
+                        self.i += 2
                     elif opcode == 4:  # as output 4, 50 => 4 output the value at address 50
-                        debug = "output"
                         # print("as input 3, 50 => 3 take input and save to address 50")
-                        i += 2
-                        value = code[first_param]
-                        return value, i, code
+                        self.i += 2
+                        output = self.code[first_param]
+                        break
                     elif opcode == 5:  # jump-if-true
-                        debug = "j true"
-                        if code[first_param] != 0:
-                            i = code[second_param]
+                        if self.code[first_param] != 0:
+                            self.i = self.code[second_param]
                         else:
-                            i += 3
+                            self.i += 3
                     elif opcode == 6:  # jump-if-false
-                        debug = "j false"
-                        if code[first_param] == 0:
-                            i = code[second_param]
+                        if self.code[first_param] == 0:
+                            self.i = self.code[second_param]
                         else:
-                            i += 3
+                            self.i += 3
                     elif opcode == 7:  # less than
-                        debug = "less"
-                        if code[first_param] < code[second_param]:
-                            code[third_param] = 1
+                        if self.code[first_param] < self.code[second_param]:
+                            self.code[third_param] = 1
                         else:
-                            code[third_param] = 0
-                        i += 4
+                            self.code[third_param] = 0
+                        self.i += 4
                     elif opcode == 8:  # equals
-                        debug = "equals"
-                        if code[first_param] == code[second_param]:
-                            code[third_param] = 1
+                        if self.code[first_param] == self.code[second_param]:
+                            self.code[third_param] = 1
                         else:
-                            code[third_param] = 0
-                        i += 4
+                            self.code[third_param] = 0
+                        self.i += 4
                     elif opcode == 9:  # relative mode
-                        rb += code[first_param]
-                        i += 2
+                        self.rb += self.code[first_param]
+                        self.i += 2
                     else:
-                        i += 1
-                    #print(i)
-                    # print(i, code)
-            except Exception as e:
-                print(e)
-                print("exception :", i, code[i-2:i+2], debug)
-                return value, i, code
-
-            return value, i, code
+                        self.i += 1
+                return output
 
         code = [int(val) for val in text.split(",")]
         nb_computers = 50
         computers = []
         i_value = 0
-        print(process(0, 0, i_value, code, False))
+        First_computer = Intcode(code)
+        print(First_computer.run(255))
         """for i in range(nb_computers):
             current_value, current_code, current_i = process(0, i, i_value, code, False)
             computers.append((i, current_code, current_i))
--- 2019/Day 9/Part two/TestDay9part2.py
+++ 2019/Day 9/Part two/TestDay9part2.py
@@ -25,136 +25,103 @@ class TestDay9part1(unittest.TestCase):
         # res = output_file()
         # pred = day_2_part_1(text)
 
-        def process(phase_setting, value, i, code, phase):
-            length = len(code)
-            rb = 0
-            end = False
-            debug = ""
-            try:
-                while i < length:
-                    # add mode on opcode
-                    print("i", i, length, code[i:i+10])
-                    if code[i] == 99:
-                        #print("exit")
-                        end = True
+        class Intcode:
+            def __init__(self, code, i=0, phase=False, rb=0):
+                self.code = code + [0] * 100000
+                self.i = i
+                self.phase = phase
+                self.rb = rb
+
+            def run(self, input, phase_setting=None, output=None):
+                if phase_setting is not None:
+                    self.phase = True
+                while True:
+                    if self.code[self.i] == 99:
                         break
-                    debug = "before cast " + str(i) + " " + str(code[i]) + " " + str(len(code))
-                    tmp_cur = code[i]
-                    str_opcode = str(tmp_cur)
+                    str_opcode = str(self.code[self.i])
                     while len(str_opcode) < 5:
                         str_opcode = "0" + str_opcode
                     opcode = int(str_opcode[3:])
-                    # add pos and imm
+                    # add pos and imm and rela
                     # case param 1
                     if int(str_opcode[2]) == 0:  # position
-                        first_param = code[i + 1]
+                        first_param = self.code[self.i + 1]
                     elif int(str_opcode[2]) == 2:  # relative
-                        #print("relative mode", str(i + 1 + rb))
-                        if i + 2 + rb > length and code[i] != 4:
-                            code += [0] * (code[i + 1] + rb - len(code))
-                        #debug = "here " + str(i + 1 + rb) + " " + str(len(code))
-                        first_param = code[i + 1] + rb
+                        first_param = self.code[self.i + 1] + self.rb
                     else:
-                        first_param = i + 1
-                    debug = "first param " + str(first_param)
+                        first_param = self.i + 1
                     # case param 2
                     if int(str_opcode[1]) == 0:  # position
-                        second_param = code[i + 2]
+                        second_param = self.code[self.i + 2]
                     elif int(str_opcode[1]) == 2:  # relative
                         #print("relative mode")
-                        second_param = code[i + 2] + rb
+                        second_param = code[self.i + 2] + self.rb
                     else:
-                        second_param = i + 2
-
-                    debug = "second param " + str(second_param)
-                    # case param 2
+                        second_param = self.i + 2
+                    # case param 3
                     if int(str_opcode[0]) == 0:  # position
-                        try:
-                            third_param = code[i + 3]
-                        except:
-                            third_param = 0
-                        debug = " third param " + str(third_param)
-                        debug += "\ncode[i] " + str(code[i])
-                        # TODO dynamic list extension here
-                        if third_param > length and code[i] != 4:
-                            code += [0] * (third_param - len(code))
+                        third_param = self.code[self.i + 3]
                     elif int(str_opcode[0]) == 2:  # relative
-                        third_param = code[i + 3] + rb
+                        third_param = self.code[self.i + 3] + self.rb
                     else:
-                        third_param = i + 3
-
-                    debug = "after params " + str(third_param) + " " + str(first_param) + " + " + str(second_param)
+                        third_param = self.i + 3
 
                     if opcode == 1:  # addition
-                        # case param 3
-                        debug = "addition " + str(third_param) + " " + str(first_param) + " + " + str(second_param) + " "\
-                                + str(len(code)) + " " + str(third_param) + " " + str(len(code) - third_param)
-                        tmp = code[first_param] + code[second_param]
-                        code[third_param] = tmp
-                        i += 4
+                        tmp = self.code[first_param] + self.code[second_param]
+                        self.code[third_param] = tmp
+                        self.i += 4
                     elif opcode == 2:  # multiplication
                         # case param 3
-                        debug = "multiplication"
-                        tmp = code[first_param] * code[second_param]
-                        code[third_param] = tmp
-                        i += 4
-                    # TODO code 3 and code 4
+                        tmp = self.code[first_param] * self.code[second_param]
+                        self.code[third_param] = tmp
+                        self.i += 4
                     elif opcode == 3:  # as input 3, 50 => 3 take input and save to address 50
-                        debug = "input"
-                        if phase:
-                            code[first_param] = phase_setting
-                            phase = False
+                        if self.phase:
+                            self.code[first_param] = phase_setting
+                            self.phase = False
                         else:
-                            code[first_param] = value
-                        i += 2
+                            self.code[first_param] = input
+                        self.i += 2
                     elif opcode == 4:  # as output 4, 50 => 4 output the value at address 50
-                        debug = "output"
                         # print("as input 3, 50 => 3 take input and save to address 50")
-                        i += 2
-                        value = code[first_param]
-                        return value, i, end, code, phase
+                        self.i += 2
+                        output = self.code[first_param]
+                        return output
                     elif opcode == 5:  # jump-if-true
-                        debug = "j true"
-                        if code[first_param] != 0:
-                            i = code[second_param]
+                        if self.code[first_param] != 0:
+                            self.i = self.code[second_param]
                         else:
-                            i += 3
+                            self.i += 3
                     elif opcode == 6:  # jump-if-false
-                        debug = "j false"
-                        if code[first_param] == 0:
-                            i = code[second_param]
+                        if self.code[first_param] == 0:
+                            self.i = self.code[second_param]
                         else:
-                            i += 3
+                            self.i += 3
                     elif opcode == 7:  # less than
-                        debug = "less"
-                        if code[first_param] < code[second_param]:
-                            code[third_param] = 1
+                        if self.code[first_param] < self.code[second_param]:
+                            self.code[third_param] = 1
                         else:
-                            code[third_param] = 0
-                        i += 4
+                            self.code[third_param] = 0
+                        self.i += 4
                     elif opcode == 8:  # equals
-                        debug = "equals"
-                        if code[first_param] == code[second_param]:
-                            code[third_param] = 1
+                        if self.code[first_param] == self.code[second_param]:
+                            self.code[third_param] = 1
                         else:
-                            code[third_param] = 0
-                        i += 4
+                            self.code[third_param] = 0
+                        self.i += 4
                     elif opcode == 9:  # relative mode
-                        rb += code[first_param]
-                        i += 2
+                        self.rb += self.code[first_param]
+                        self.i += 2
                     else:
-                        i += 1
-                    #print(i)
-                    # print(i, code)
-            except Exception as e:
-                print(e)
-                print(i, code[i-2:i+2], debug)
-                return value, i, end, code, phase
-
-            return value, i, end, code, phase
+                        self.i += 1
+                return output
 
         code = [int(val) for val in text.split(",")]
-        print(process(0, 2, 0, code, False))
+        #print(process(0, 1, 0, code, False))
+
+
+        First_computer = Intcode(code)
+        print(First_computer.run(2))
 
 
 if __name__ == '__main__':
