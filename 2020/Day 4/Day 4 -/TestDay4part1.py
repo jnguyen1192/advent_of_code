7d38b0a384b4697c1d88bbce1764bdf1b078b315
Johnny Nguyen <johdu13@hotmail.fr>
2020-12-04 22:07:17 +0100

    #6 Day 4 done commented in Python

--- 2020/Day 4/Day 4 -/TestDay4part1.py
+++ 2020/Day 4/Day 4 -/TestDay4part1.py
@@ -19,37 +19,37 @@ def output_file():
 def get_nb_valid_password(lines):
     def is_valid_password(fields_valid, password_to_check):
         nb_key_in_password = 0
-        for key in password_to_check:
-            if key in fields_valid:
-                nb_key_in_password += 1
-        if nb_key_in_password >= len(fields_valid):  # low
+        for key in password_to_check:  # for each keys
+            if key in fields_valid:  # is key valid
+                nb_key_in_password += 1  # increment nb key
+        if nb_key_in_password >= len(fields_valid):  # test if nb key is correct
             return True
         return False
 
 
-    fields_valid = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-    password_to_check = {}
-    passwords_to_check = []
-    # cid
-    for line in lines:
-        for code in line.split(" "):
-            if line == "":
-                passwords_to_check.append(password_to_check)
-                password_to_check = {}
-                continue
-            key, value = code.split(":")
-            password_to_check[key] = value
-    passwords_to_check.append(password_to_check)
-    nb_valid_password = 0
-    nb_password = 0
-    for password_to_check in passwords_to_check:
-        nb_password += 1
-        if is_valid_password(fields_valid, password_to_check):
-            nb_valid_password += 1
-    # password last check
-    print("nb_password", nb_password)
+    fields_valid = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]  # all key present and valid
 
-    return nb_valid_password#0#nb_tree_encounter_product  # case it won't work
+    password_to_check = {}  # initialize a password
+    passwords_to_check = []  # initialize password list
+
+    for line in lines:  # for each line
+        for code in line.split(" "):  # get the keys
+            if line == "":  # case it is a blank line
+                passwords_to_check.append(password_to_check)  # add a new password on list
+                password_to_check = {}  # clear the password
+                continue
+            key, value = code.split(":")  # extract key and value
+            password_to_check[key] = value  # add a new value on dict with its key
+    passwords_to_check.append(password_to_check)  # add the last new password on list
+
+    nb_valid_password = 0  # initialize nb valid password
+    #nb_password = 0
+    for password_to_check in passwords_to_check:  # for each passwords
+        #nb_password += 1
+        if is_valid_password(fields_valid, password_to_check):  # check if it is valid
+            nb_valid_password += 1  # increment nb valid password
+    # password last check
+    return nb_valid_password  # return nb of valid password
 
 
 class TestDay4part1(unittest.TestCase):
