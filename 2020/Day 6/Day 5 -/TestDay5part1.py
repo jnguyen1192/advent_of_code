72455100705e9f649baef08082e2a1d6a4d7be59
Johnny Nguyen <johdu13@hotmail.fr>
2020-12-06 06:57:41 +0100

    #6 Day 6 done, commented and refactored in Python

---
+++ 2020/Day 6/Day 5 -/TestDay5part1.py
@@ -0,0 +1,46 @@
+import unittest
+
+
+def input_file():
+    file = open('input', 'r')
+    lines = [line.rstrip('\n') for line in file]
+    file.close()
+    return lines
+
+
+def output_file():
+    file = open('output', 'r')
+    res = [line.rstrip('\n') for line in file]
+    file.close()
+    return res
+
+
+def get_sum_group_count_yes(lines):
+    letters = "abcdefghijklmnopqrstuvwxyz"  # initialize all char to count
+    group_yes = []  # initialize group of yes
+    group_count = {}  # initialize each group letter count
+    for char in letters:  # for each letter
+        group_count[char] = 0  # initialize count as 0
+    for line in lines:  # for each people
+        for char in line:  # for each yes answer
+            group_count[char] = 1  # set letter count to one
+        if line == "":
+            group_yes.append(sum(group_count.values()))  # sum the values of yes and add it to the final group to count yes
+            group_count = {}  # clear group yes count
+    group_yes.append(sum(group_count.values()))  # sum the values of yes and add it to the final group to count yes
+
+    return sum(group_yes)  # return nb of valid password
+
+
+class TestDay5part1(unittest.TestCase):
+
+    def test_day_5_part_1(self):
+        lines = input_file()  # get input
+        res = output_file()  # get output
+        pred = get_sum_group_count_yes(lines)  # process
+        print(pred)  # print
+        assert(str(pred) == res[0])  # check
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 2020/Day 6/Part two/TestDay5part2.py
@@ -0,0 +1,57 @@
+import unittest
+from collections import Counter
+
+
+def input_file():
+    file = open('input', 'r')
+    lines = [line.rstrip('\n') for line in file]
+    file.close()
+    return lines
+
+
+def output_file():
+    file = open('output', 'r')
+    res = [line.rstrip('\n') for line in file]
+    file.close()
+    return res
+
+
+def get_sum_group_count_yes(lines):
+    letters = "abcdefghijklmnopqrstuvwxyz"  # initialize all char to count
+    group_yes = []  # initialize group of yes
+    group_count = dict()  # initialize each group letter count
+    group_count["nb_group"] = 0
+
+    def yes_all(group_count):
+        for key in group_count:  # for each answer
+            if group_count["nb_group"] == group_count[key] and key != "nb_group":  # if the number of yes equals number of people
+                yield 1  # return 1
+    for char in letters:  # for each letter
+        group_count[char] = 0  # initialize count as 0
+    for line in lines:  # for each people
+        for char in line:  # for each yes answer
+            group_count[char] += 1  # increment letter count
+        if line == "":
+            group_yes.append(sum(yes_all(group_count)))  # part 2 sum the values of all yes and add it to the final group to count yes
+            for char in letters:  # for each letter
+                group_count[char] = 0  # set char value to 0
+            group_count["nb_group"] = 0  # set the value to nb group to 0
+        else:
+            group_count["nb_group"] += 1  # increment nb_group
+    group_yes.append(sum(yes_all(group_count)))  # get the sum of all yes for each group
+
+    return sum(group_yes)  # return nb of valid password
+
+
+class TestDay5part1(unittest.TestCase):
+
+    def test_day_5_part_1(self):
+        lines = input_file()  # get input
+        res = output_file()  # get output
+        pred = get_sum_group_count_yes(lines)  # process
+        print(pred)  # print
+        assert(str(pred) == res[0])  # check
+
+
+if __name__ == '__main__':
+    unittest.main()
