e1ed674c36ed905dc2ac443f65c8e3fc5742ba63
Johnny Nguyen <johdu13@hotmail.fr>
2020-12-08 08:01:07 +0100

    #6 Day 8 refactored and commented in Python

--- 2020/Day 8/Day 8 -/TestDay8part1.py
+++ 2020/Day 8/Day 8 -/TestDay8part1.py
@@ -16,33 +16,42 @@ def output_file():
 
 
 def get_accumulator(lines):
-    accumulator = 0  # initialize accumlator
-    instructions = []  # initialize instructions
 
-    def compiler(i, instr, value, accumulator):  # analyser if each instructions
-        if instr == "nop":  # case nop
-            return i + 1, accumulator  # go to next instruction
-        if instr == "acc":  # case acc
-            accumulator += value  # increase accumulator value
-            return i + 1, accumulator  # go to next instruction
-        if instr == "jmp":
-            return i + value, accumulator  # go to +value instruction
-        return "error"
+    class game_console:
+        def __init__(self):
+            self.accumulator = 0  # initialize accumlator
+            self.instructions = []  # initialize instructions
+            self.history = {"i": []}  # initialize history
 
-    for line in lines:  # for each linse
-        instr, number_raw = line.split(" ")  # extract the instruction and value
-        instructions.append((instr, int(number_raw[1:]) if number_raw[0]=="+" else -int(number_raw[1:])))  # add them into instruction list
-    #print(instructions)
+        def prepare_instructions(self,lines):
+            for line in lines:  # for each linse
+                instr, number_raw = line.split(" ")  # extract the instruction and value
+                self.instructions.append((instr, int(number_raw[1:]) if number_raw[0] == "+" else -int(
+                    number_raw[1:])))  # add them into instruction list
 
-    history = {"i": []}  # initialize history
-    i = 0  # initialize line number in instruction list
-    while True:
-        history["i"].append(i)  # add line number in history line number
-        i, accumulator = compiler(i, instructions[i][0], instructions[i][1], accumulator)
-        if i in history["i"]:  # case line instruction was already travelled
-            break  # end the program
+        def compiler(self, i, instr, value, accumulator):  # analyser if each instructions
+            if instr == "nop":  # case nop
+                return i + 1, accumulator  # go to next instruction
+            if instr == "acc":  # case acc
+                accumulator += value  # increase accumulator value
+                return i + 1, accumulator  # go to next instruction
+            if instr == "jmp":
+                return i + value, accumulator  # go to +value instruction
+            return "error"
 
-    return accumulator  # return nb of valid password
+        def run(self):
+            i = 0  # initialize line number in instruction list
+            while True:
+                self.history["i"].append(i)  # add line number in history line number
+                i, self.accumulator = self.compiler(i, self.instructions[i][0], self.instructions[i][1], self.accumulator)
+                if i in self.history["i"]:  # case line instruction was already travelled
+                    break  # end the program
+
+    g_c = game_console()  # create game console
+    g_c.prepare_instructions(lines)  # insert instructions in game console
+    g_c.run()  # start game console
+
+    return g_c.accumulator  # return accumlator in game console
 
 
 class TestDay8part1(unittest.TestCase):
--- 2020/Day 8/Part two/TestDay8part2.py
+++ 2020/Day 8/Part two/TestDay8part2.py
@@ -16,52 +16,63 @@ def output_file():
 
 
 def get_accumulator(lines):
-    accumulator = 0
-    instructions = []
+    class game_console:
+        def __init__(self):
+            self.accumulator = 0  # initialize accumlator
+            self.instructions = []  # initialize instructions
+            self.history = {"i": [],
+                            "wrong_instr": [],
+                            "correct_value": False}  # initialize history
 
-    def compiler(i, instr, value, accumulator, history):
-        if instr == "nop":
-            if i not in history["wrong_instr"] and not history["correct_value"]:
-                history["wrong_instr"].append(i)
-                history["correct_value"] = True
-                return i + value, accumulator, history
-            return i + 1, accumulator, history
-        if instr == "acc":
-            accumulator += value
-            return i + 1, accumulator, history
-        if instr == "jmp":
-            if i not in history["wrong_instr"] and not history["correct_value"]:
-                history["wrong_instr"].append(i)
-                history["correct_value"] = True
-                return i + 1, accumulator, history
-            return i + value, accumulator, history
-        return "error"
+        def prepare_instructions(self, lines):
+            for line in lines:  # for each linse
+                instr, number_raw = line.split(" ")  # extract the instruction and value
+                self.instructions.append((instr, int(number_raw[1:]) if number_raw[0] == "+" else -int(
+                    number_raw[1:])))  # add them into instruction list
 
-    for line in lines:
-        instr, number_raw = line.split(" ")
-        instructions.append((instr, int(number_raw[1:]) if number_raw[0]=="+" else -int(number_raw[1:])))
-    #print(instructions)
-    history = {"i": [],
-               "wrong_instr": [],
-               "correct_value": False}
-    i = 0
+        def compiler(self, i, instr, value, accumulator):  # analyser if each instructions
+            if instr == "nop":  # case nop
+                if i not in self.history["wrong_instr"] and not self.history["correct_value"]:  # case correct nop instruction
+                    self.history["wrong_instr"].append(i)  # add line correct instruction into history
+                    self.history["correct_value"] = True  # exit corrected mode
+                    return i + value, accumulator, self.history  # go to +value instruction
+                return i + 1, accumulator, self.history  # go to next instruction
+            if instr == "acc":  # case acc
+                accumulator += value  # increase accumulator value
+                return i + 1, accumulator, self.history  # go to next instruction
+            if instr == "jmp":
+                if i not in self.history["wrong_instr"] and not self.history["correct_value"]:  # case correct jmp instruction
+                    self.history["wrong_instr"].append(i)  # add line correct instruction into history
+                    self.history["correct_value"] = True  # exit corrected mode
+                    return i + 1, accumulator, self.history  # go to next instruction
+                return i + value, accumulator, self.history  # go to +value instruction
+            return "error"
 
-    while True:
-        history["i"].append(i)
-        i, accumulator, history = compiler(i, instructions[i][0], instructions[i][1], accumulator, history)
-        #print(history["i"], history["wrong_instr"])
-        if i in history["i"]:
-            # infinite loop
-            i = 0
-            history["i"] = []
-            history["correct_value"] = False
-            accumulator = 0
-            #print("Infinite loop")
-        if i == len(instructions):
-            #print("Correct end")
-            break
+        def run(self):
+            i = 0  # initialize line number in instruction list
+            while True:
+                self.history["i"].append(i)  # add line number in history line number
+                i, self.accumulator, self.history = self.compiler(i, self.instructions[i][0],
+                                                                  self.instructions[i][1],
+                                                                  self.accumulator)
 
-    return accumulator  # return nb of valid password
+                if i in self.history["i"]:  # case line instruction was already travelled
+                    # infinite loop
+                    i = 0  # restart the program
+                    self.history["i"] = []
+                    self.history["correct_value"] = False  # correct next nop or jmp instruction
+                    self.accumulator = 0  # restart accumulator
+                    # print("Infinite loop")
+                if i == len(self.instructions):
+                    # print("Correct end")
+                    break
+
+    g_c = game_console()  # create game console
+    g_c.prepare_instructions(lines)  # insert instructions in game console
+    g_c.run()  # start game console
+
+    return g_c.accumulator  # return accumulator in game console
+
 
 
 class TestDay8part1(unittest.TestCase):
