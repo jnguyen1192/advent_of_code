622ff8b772dbde341fa21c454dc844ec826d70c3
jnguyen1192 <johdu13@hotmail.fr>
2021-12-02 07:37:25 +0100

    # 2021 day 2

--- 2021/Day 2/Day 2 -/TestDay2part1.py
+++ 2021/Day 2/Day 2 -/TestDay2part1.py
@@ -1,5 +1,4 @@
 import unittest
-from collections import Counter
 
 
 def input_file():
@@ -16,21 +15,18 @@ def output_file():
     return res
 
 
-def get_nb_pass_valid(lines):
-    nb_pass = 0  # initialize count valid pass
+def get_final_position(lines):
+    cmd_dict = {"forward": (1,0),
+           "down": (0, 1),
+           "up": (0, -1)}
+    horizontal_depht = (0, 0)
     for line in lines:  # for each lines
-        raw_policy, raw_letter, password = line.split(" ")  # extract policy
-        min_policy, max_policy = raw_policy.split("-")
-        letter = raw_letter[0]  # the letter to test
-        c = 0  # initialize count
-        for letter_password in password:  # for each letter in password
-            if letter == letter_password:  # case there was the letter to test in password
-                c += 1  # increment count
-        #print(c)
-        if int(min_policy) <= c <= int(max_policy):
-            #print(letter, "policy ok")
-            nb_pass += 1  # increment count valid pass
-    return nb_pass  # case it won't work
+        cmd, X = line.split()
+        X = int(X)
+        horizontal, depht = (horizontal_depht[0] + cmd_dict[cmd][0] * X, horizontal_depht[1] + cmd_dict[cmd][1] * X)
+        #print(horizontal_depht)
+        horizontal_depht =  (horizontal, depht)
+    return horizontal_depht[0] * horizontal_depht[1]  # case it won't work
 
 
 class TestDay2part1(unittest.TestCase):
@@ -38,7 +34,7 @@ class TestDay2part1(unittest.TestCase):
     def test_day_2_part_1(self):
         lines = input_file()  # get input_test
         res = output_file()  # get output_1
-        pred = get_nb_pass_valid(lines)  # process
+        pred = get_final_position(lines)  # process
         print(pred)  # print
         assert(str(pred) == res[0])  # check
 
--- 2021/Day 2/Part two/TestDay2part2.py
+++ 2021/Day 2/Part two/TestDay2part2.py
@@ -1,5 +1,4 @@
 import unittest
-from collections import Counter
 
 
 def input_file():
@@ -16,22 +15,19 @@ def output_file():
     return res
 
 
-def get_nb_pass_valid(lines):
-    nb_pass = 0  # initialize count valid pass
+def get_final_position(lines):
+    cmd_dict = {"forward": (1, 0),
+           "down": (0, 1),
+           "up": (0, -1)}
+    horizontal_depht = (0, 0)
+    aim = 0
     for line in lines:  # for each lines
-        raw_policy, raw_letter, password = line.split(" ")  # extract policy
-        min_policy, max_policy = raw_policy.split("-")
-        letter = raw_letter[0]  # the letter to test
+        cmd, X = line.split()
+        X = int(X)
+        aim += cmd_dict[cmd][1] * X
+        horizontal_depht = (horizontal_depht[0] + cmd_dict[cmd][0] * X, horizontal_depht[1] + cmd_dict[cmd][0] * aim * X)
 
-        c = 0  # initialize count
-        #for letter_password in password:  # for each letter in password
-        if letter == password[int(min_policy)-1]:  # case there was the letter to test in min policy
-            c += 1  # increment count
-        if letter == password[int(max_policy)-1]:  # case there was the letter to test in min policy
-            c += 1  # increment count
-        if c == 1:
-            nb_pass += 1
-    return nb_pass  # case it won't work
+    return horizontal_depht[0] * horizontal_depht[1]  # case it won't work
 
 
 class TestDay2part2(unittest.TestCase):
@@ -39,7 +35,7 @@ class TestDay2part2(unittest.TestCase):
     def test_day_2_part_2(self):
         lines = input_file()  # get input_test
         res = output_file()  # get output_1
-        pred = get_nb_pass_valid(lines)  # process
+        pred = get_final_position(lines)  # process
         print(pred)  # print
         assert(str(pred) == res[0])  # check
 
